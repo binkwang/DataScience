d6bcc0b69de094ffb1905980ca3fa7740149a9df
hanmbink <fxmmc@sina.com>
2021-06-25 16:33:56 +1200

    refactor

---
+++ streamlit_app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os, urllib
+import streamlit_features_test, uber_pickups
+
+# Path to the Streamlit public S3 bucket
+DATA_URL_ROOT = "https://s3-us-west-2.amazonaws.com/"
+
+def main():
+
+    # Render the readme as markdown using st.markdown.
+
+    readme_text = st.markdown(get_file_content_as_string("instructions.md"))
+
+    # Add a selector for the app mode on the sidebar.
+
+    st.sidebar.title("What to do")
+
+    app_mode = st.sidebar.selectbox("Choose the app mode",
+        ["Show instructions", "App 1", "App 2"])
+
+    if app_mode == "Show instructions":
+        st.sidebar.success('To continue select an app from drop down list')
+    
+    elif app_mode == "App 1":
+        readme_text.empty()
+        streamlit_features_test.run_it()
+    
+    elif app_mode == "App 2":
+        readme_text.empty()
+        uber_pickups.run_it()
+
+# Download a single file and make its content available as a string.
+@st.cache(show_spinner=False)
+def get_file_content_as_string(path):
+	url = 'https://raw.githubusercontent.com/hanmbink/DataScience/main/' + path
+	response = urllib.request.urlopen(url)
+	return response.read().decode("utf-8")
+
+if __name__ == "__main__":
+    main()
--- uber_pickups.py
+++ uber_pickups.py
@@ -18,53 +18,55 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
+def run_it():
 
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+	# Create a text element and let the reader know the data is loading.
+	data_load_state = st.text('Loading data...')
 
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+	# Load 10,000 rows of data into the dataframe.
+	data = load_data(10000)
 
-if st.checkbox('Show raw data'):
-	st.subheader('Raw data')
+	# Notify the reader that the data was successfully loaded.
+	data_load_state.text('Loading data...done!')
 
-	# st.write tries to do the right thing 
-	# based on the data type of the input.
-	# In this case, you’re passing in a dataframe 
-	# and it’s rendering as an interactive table.
-	st.write(data) 
+	if st.checkbox('Show raw data'):
+		st.subheader('Raw data')
 
-# Or you can use a specialized command like st.dataframe instead
-# to render the dataframe.
-# st.dataframe(data)
+		# st.write tries to do the right thing 
+		# based on the data type of the input.
+		# In this case, you’re passing in a dataframe 
+		# and it’s rendering as an interactive table.
+		st.write(data) 
 
-
-"""
-## Number of pickups by hour
-"""
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, 
-    bins=24, 
-    range=(0,24))[0]
-st.bar_chart(hist_values)
+	# Or you can use a specialized command like st.dataframe instead
+	# to render the dataframe.
+	# st.dataframe(data)
 
 
-"""
-## Map of all pickups
-"""
-st.map(data)
+	"""
+	## Number of pickups by hour
+	"""
 
-"""
-## Map of pickups in a selected time
-"""
+	hist_values = np.histogram(
+	    data[DATE_COLUMN].dt.hour, 
+	    bins=24, 
+	    range=(0,24))[0]
+	st.bar_chart(hist_values)
 
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+
+	"""
+	## Map of all pickups
+	"""
+	st.map(data)
+
+	"""
+	## Map of pickups in a selected time
+	"""
+
+	hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+	filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+	st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+	# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+	st.map(filtered_data)
 
 
